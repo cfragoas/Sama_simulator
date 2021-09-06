2223047c1e9c8c283b57d5425573095ede8fe34e
cfragoas <christianfragoas@gmail.com>
2021-09-06 14:21:17 -0300

    first iteration of UE capacity calcullation (not very good)

--- main_test_codes/beamform_test.py
+++ main_test_codes/beamform_test.py
@@ -24,7 +24,7 @@ ue = User_eq(positions=grid.grid, height=1.5)  #creating the user equipament obj
 element = Element_ITU2101(max_gain=5, phi_3db=65, theta_3db=65, front_back_h=30, sla_v=30, plot=False)
 beam_ant = Beamforming_Antenna(ant_element=element, frequency=10, n_rows=8, n_columns=8, horizontal_spacing=0.5,
                                   vertical_spacing=0.5)
-bs = BaseStation(frequency=3.5, tx_power=10, tx_height=30, bw=20, n_sectors=3, antenna=beam_ant, gain=10, downtilts=0,
+bs = BaseStation(frequency=3.5, tx_power=10, tx_height=30, bw=100, n_sectors=3, antenna=beam_ant, gain=10, downtilts=0,
                  plot=False)
 bs.sector_beam_pointing_configuration(n_beams=10)  # configuring the base stations to use 10 beams each
 cluster = Cluster()
