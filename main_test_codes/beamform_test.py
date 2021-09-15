c9e8215c4d79de4b8a6a05f870912da1fb535d43
cfragoas <christianfragoas@gmail.com>
2021-09-15 12:36:53 -0300

    first interation of macel increment test

--- main_test_codes/beamform_test.py
+++ main_test_codes/beamform_test.py
@@ -20,53 +20,60 @@ from user_eq import User_eq
 
 
 # testing a base station with beamforming
-mean_snr = []
-std_snr = []
-mean_cap = []
-std_cap = []
-for i in range(100):
-    grid = Grid()  # grid object
-    grid.make_grid(1000, 1000)  # creating a grid with x, y dimensions
-    grid.make_points(dist_type='gaussian', samples=10, n_centers=4, random_centers=False, plot=False)  # distributing points aring centers in the grid
-    ue = User_eq(positions=grid.grid, height=1.5)  #creating the user equipament object
-    element = Element_ITU2101(max_gain=5, phi_3db=65, theta_3db=65, front_back_h=30, sla_v=30, plot=False)
-    beam_ant = Beamforming_Antenna(ant_element=element, frequency=10, n_rows=8, n_columns=8, horizontal_spacing=0.5,
-                                      vertical_spacing=0.5)
-    bs = BaseStation(frequency=3.5, tx_power=50, tx_height=30, bw=300, n_sectors=3, antenna=beam_ant, gain=10, downtilts=0,
-                     plot=False)
-    bs.sector_beam_pointing_configuration(n_beams=10)  # configuring the base stations to use 10 beams each
-    cluster = Cluster()
-    cluster.k_means(grid=grid.grid, n_clusters=2)
-    lines = grid.lines
-    columns = grid.columns
-    az_map = generate_azimuth_map(lines=lines, columns=columns, centroids=cluster.centroids,samples=cluster.features)
-    dist_map = generate_euclidian_distance(lines=lines, columns=columns, centers=cluster.centroids, samples=cluster.features, plot=False)
-    elev_map = generate_elevation_map(htx=30, hrx=1.5, d_euclid=dist_map, cell_size=30, samples=None)
-    bs.beam_configuration(az_map=bs.beams_pointing)  # creating a beamforming configuration pointing to the the az_map points
-    # bs.beam_configuration(az_map=az_map[0], elev_map=elev_map[0])  # rever essa parada aqui!!!
-    # base_station_list = [bs] # creating a list is this case because theres is only one BS
-    # gain_map = generate_bf_gain(elevation_map=elev_map, azimuth_map=az_map, base_station_list=base_station_list, sector_index=0)
+for n_centers in range(1,20):
+    mean_snr = []
+    std_snr = []
+    mean_cap = []
+    std_cap = []
+    for i in range(100):
+        grid = Grid()  # grid object
+        grid.make_grid(1000, 1000)  # creating a grid with x, y dimensions
+        grid.make_points(dist_type='gaussian', samples=20, n_centers=4, random_centers=False, plot=False)  # distributing points aring centers in the grid
+        ue = User_eq(positions=grid.grid, height=1.5)  #creating the user equipament object
+        element = Element_ITU2101(max_gain=5, phi_3db=65, theta_3db=65, front_back_h=30, sla_v=30, plot=False)
+        beam_ant = Beamforming_Antenna(ant_element=element, frequency=10, n_rows=8, n_columns=8, horizontal_spacing=0.5,
+                                          vertical_spacing=0.5)
+        bs = BaseStation(frequency=3.5, tx_power=50, tx_height=30, bw=300, n_sectors=3, antenna=beam_ant, gain=10, downtilts=0,
+                         plot=False)
+        bs.sector_beam_pointing_configuration(n_beams=10)  # configuring the base stations to use 10 beams each
+        cluster = Cluster()
+        cluster.k_means(grid=grid.grid, n_clusters=n_centers)
+        lines = grid.lines
+        columns = grid.columns
+        az_map = generate_azimuth_map(lines=lines, columns=columns, centroids=cluster.centroids,samples=cluster.features)
+        dist_map = generate_euclidian_distance(lines=lines, columns=columns, centers=cluster.centroids, samples=cluster.features, plot=False)
+        elev_map = generate_elevation_map(htx=30, hrx=1.5, d_euclid=dist_map, cell_size=30, samples=None)
+        bs.beam_configuration(az_map=bs.beams_pointing)  # creating a beamforming configuration pointing to the the az_map points
+        # bs.beam_configuration(az_map=az_map[0], elev_map=elev_map[0])  # rever essa parada aqui!!!
+        # base_station_list = [bs] # creating a list is this case because theres is only one BS
+        # gain_map = generate_bf_gain(elevation_map=elev_map, azimuth_map=az_map, base_station_list=base_station_list, sector_index=0)
 
-    macel = Macel(grid=grid, n_centers=2, prop_model='free space', criteria=0, cell_size=30, base_station=bs)
-    macel.generate_base_station_list()
-    macel.set_ue(ue=ue)
-    ch_gain_map, sector_map = macel.generate_bf_gain_maps(az_map=az_map, elev_map=elev_map, dist_map=dist_map)
-    macel.ue.acquire_bs_and_beam(ch_gain_map=ch_gain_map, sector_map=sector_map)  # calculating the best ch gain for each UE
-    # macel.simulate_ue_bs_comm(simulation_time=1, time_slot=1)
-    macel.send_ue_to_bs(simulation_time=1000, time_slot=1)
-    snr_cap_stats = macel.simulate_ue_bs_comm(ch_gain_map=ch_gain_map)
-    mean_snr.append(snr_cap_stats[0])
-    std_snr.append(snr_cap_stats[1])
-    mean_cap.append(snr_cap_stats[2])
-    std_cap.append(snr_cap_stats[3])
+        macel = Macel(grid=grid, n_centers=n_centers, prop_model='free space', criteria=0, cell_size=30, base_station=bs)
+        macel.generate_base_station_list()
+        macel.set_ue(ue=ue)
+        ch_gain_map, sector_map = macel.generate_bf_gain_maps(az_map=az_map, elev_map=elev_map, dist_map=dist_map)
+        macel.ue.acquire_bs_and_beam(ch_gain_map=ch_gain_map, sector_map=sector_map)  # calculating the best ch gain for each UE
+        # macel.simulate_ue_bs_comm(simulation_time=1, time_slot=1)
+        macel.send_ue_to_bs(simulation_time=1000, time_slot=1)
+        snr_cap_stats = macel.simulate_ue_bs_comm(ch_gain_map=ch_gain_map)
+        mean_snr.append(snr_cap_stats[0])
+        std_snr.append(snr_cap_stats[1])
+        mean_cap.append(snr_cap_stats[2])
+        std_cap.append(snr_cap_stats[3])
 
-plt.plot(mean_snr)
-plt.show()
-plt.plot(std_snr)
-plt.show()
-plt.plot(stats.norm.pdf(snr_cap_stats[0], np.mean(snr_cap_stats[0]), np.std(snr_cap_stats[0])))
-plt.show()
-plt.plot(mean_cap)
-plt.show()
-plt.plot(std_cap)
-plt.show()
+    plt.plot(mean_snr)
+    plt.show()
+    plt.plot(std_snr)
+    plt.show()
+    # plt.plot(stats.norm.pdf(np.sort(mean_snr), np.mean(mean_snr), np.std(mean_snr)))
+    # plt.show()
+    # plt.plot(stats.norm.pdf(np.sort(std_snr), np.mean(std_snr), np.std(std_snr)))
+    # plt.show()
+    plt.plot(mean_cap)
+    plt.show()
+    plt.plot(std_cap)
+    plt.show()
+    # plt.plot(stats.norm.pdf(np.sort(mean_cap), np.mean(mean_cap), np.std(mean_cap)))
+    # plt.show()
+    # plt.plot(stats.norm.pdf(np.sort(std_cap), np.mean(std_cap), np.std(std_cap)))
+    # plt.show()
