ed0fdb210852cb54788a53daf2196d999b34c746
nfe88 <nickfelias@gmail.com>
2024-07-21 18:21:59 -0300

    Adicionei verificadores de existencia de arquivos em teste_raster. Depois subir para classe Raster.

--- main_test_codes/teste_raster.py
+++ main_test_codes/teste_raster.py
@@ -133,7 +133,10 @@ class Raster(Grid):
 
     def rasterize_shapefile(self,input_shapefile_path,output_raster_path):
 
-        shp = ogr.Open(input_shapefile_path) #Get_shapefile_attributes():r'RiodeJaneiro_shp\RJ_shape\buildings.shp'
+        if os.path.isfile(input_shapefile_path): #Conferindo se o shapefile selecionado existe
+            shp = ogr.Open(input_shapefile_path) #Get_shapefile_attributes():r'RiodeJaneiro_shp\RJ_shape\buildings.shp'
+        else:
+            raise Exception('Shapefile selecionado nao encontrado. Verifique novamente!')
         self.src_layer = shp.GetLayer()
 
         
@@ -142,17 +145,22 @@ class Raster(Grid):
         self.x_res = int(round((self.xmax-self.xmin)/self.pixel_size)) #Resolução X
         self.y_res = int(round((self.ymax-self.ymin)/self.pixel_size)) #Resolução Y
 
-        target_ds = gdal.GetDriverByName('GTiff').Create(output_raster_path,self.x_res,self.y_res,1,gdal.GDT_Float32,['COMPRESS=LZW']) # 1 = numbandas 
-        target_ds.SetGeoTransform((self.xmin,self.pixel_size,0.0,self.ymax,0.0,-self.pixel_size))
-        srse = osr.SpatialReference()
-        projection = 'EPSG:4326'
-        srse.SetWellKnownGeogCS(projection)
-        target_ds.SetProjection(srse.ExportToWkt()) 
-        band = target_ds.GetRasterBand(1)
-        target_ds.GetRasterBand(1).SetNoDataValue(0) # era -9999
-        band.Fill(0) #-9999
-        gdal.RasterizeLayer(target_ds,[1],self.src_layer,None,None,[1],options = ['ALL_TOUCHED=TRUE','ATRIBUTE=osm_id'])
-        target_ds = None
+        if os.path.isfile(output_raster_path): # Conferindo se o raster que será criado já existe 
+            print('Raster desejado ja existe com esse nome! Pulando a rasterizacao ...')
+            pass
+        
+        else:
+            target_ds = gdal.GetDriverByName('GTiff').Create(output_raster_path,self.x_res,self.y_res,1,gdal.GDT_Float32,['COMPRESS=LZW']) # 1 = numbandas 
+            target_ds.SetGeoTransform((self.xmin,self.pixel_size,0.0,self.ymax,0.0,-self.pixel_size))
+            srse = osr.SpatialReference()
+            projection = 'EPSG:4326'
+            srse.SetWellKnownGeogCS(projection)
+            target_ds.SetProjection(srse.ExportToWkt()) 
+            band = target_ds.GetRasterBand(1)
+            target_ds.GetRasterBand(1).SetNoDataValue(0) # era -9999
+            band.Fill(0) #-9999
+            gdal.RasterizeLayer(target_ds,[1],self.src_layer,None,None,[1],options = ['ALL_TOUCHED=TRUE','ATRIBUTE=osm_id'])
+            target_ds = None
     
     def open_rasterfile(self,output_raster_path):     
         with rasterio.open(output_raster_path) as raster_file: # Abrindo o raster -> Open_Raster():
@@ -225,7 +233,7 @@ import rasterio
 
 
 input = 'rasters/shapefiles/londres/teste_londres.shp'
-output = 'rasters/testepy_londres_debug3.tif'
+output = 'rasters/testepy_londres.tif'
 
 raster = Raster()
 
